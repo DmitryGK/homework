3f28f27a92c43ba336b8577cf197531628259321
DmitryGK <gulyakevichdmitry@gmail.com>
2022-04-10 20:49:30 +0300

    complete: SuperRange add: css

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,43 +1,48 @@
-import React, {useState} from 'react'
+import React, { ChangeEvent, ChangeEventHandler, useState } from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import s from './common/c7-SuperRange/SuperRange.module.css'
+
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
-    const onChange = () => {}
-    const onChangeValue = () => {}
+
+
+    const onChangeSlider = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue1(parseInt(e.currentTarget.value, 10))
+    }
 
     return (
 
-       
         <div>
-            <hr/>
+            <hr />
             homeworks 11
-
-            
             <div>
-                <span>{value1}</span>
+                <span className={s.rangeSliderValue}>{value1}</span>
                 <SuperRange
-                    onChange={onChange}
-                    onChangeValue={onChangeValue}
+                    min={0}
+                    max={100}
+                    step={1}
+                    value={value1}
+                    onChangeValue={onChangeSlider}
                 />
             </div>
 
             <div>
                 <span>{value1}</span>
                 <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
+
                 />
                 <span>{value2}</span>
             </div>
 
-            <hr/>
+            <hr />
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperRange/>*/}
             {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
+            <hr />
         </div>
     )
 }
--- src/p2-homeworks/h11/common/c7-SuperRange/SuperRange.tsx
+++ src/p2-homeworks/h11/common/c7-SuperRange/SuperRange.tsx
@@ -1,43 +1,45 @@
 import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
 import s from './SuperRange.module.css'
 
-// тип пропсов обычного инпута
-type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
-// здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута
-// (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
-type SuperRangePropsType = DefaultInputPropsType & { // и + ещё пропсы которых нет в стандартном инпуте
-    onChangeRange?: (value: number) => void
-};
- 
-const SuperRange: React.FC<SuperRangePropsType> = (
-    {
-        type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
-        onChange, onChangeRange,
-        className,
 
-        ...restProps// все остальные пропсы попадут в объект restProps
-    }
-) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange && onChange(e) // сохраняем старую функциональность
+type SuperRangePropsType = {
+    step: number
+    min: number
+    max: number
+    value: number
+    onChangeValue: (e:ChangeEvent<HTMLInputElement>) => void
+}
 
-        onChangeRange && onChangeRange(+e.currentTarget.value)
-    }
+const SuperRange = (props: SuperRangePropsType) => {
 
-    const finalRangeClassName = `${s.range} ${className ? className : ''}`
+  const {
+      step,
+      min,
+      max,
+      value,
+      onChangeValue
+  } = props
 
-    return (
-        <>
-            <input
-                type={'range'}
-                onChange={onChangeCallback}
-                className={finalRangeClassName}
-
-                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
-            />
-        </>
-    )
+  const handleChange = (value: number) => (e:ChangeEvent<HTMLInputElement>) => {
+      onChangeValue(e)
+  }
+    
+  return (
+      <>
+          <div className={s.sliderContainer}>
+                <input 
+                    className={s.rangeSlider}
+                    type='range'
+                    step={step}
+                    min={min}
+                    max={max}
+                    value={value}
+                    onChange={handleChange(max)}
+                />
+          </div>
+      </>
+  )   
 }
 
 export default SuperRange
