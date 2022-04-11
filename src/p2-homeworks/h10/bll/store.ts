56c6cd81f0ffd540cfda0663a160bb2882ecc24b
DmitryGK <gulyakevichdmitry@gmail.com>
2022-04-11 12:46:53 +0300

    hw12: micro changings

--- src/p2-homeworks/h10/bll/store.ts
+++ src/p2-homeworks/h10/bll/store.ts
@@ -1,10 +1,11 @@
+import { themeReducer } from './../../h12/bll/themeReducer';
 import {loadingReducer} from './loadingReducer'
 import {} from 'react-redux'
 import { combineReducers, createStore } from 'redux'
 
 const reducers = combineReducers({
     loading: loadingReducer,
-
+    theme: themeReducer
 })
 
 const store = createStore(reducers)
--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import SuperSelect from "../h7/common/c5-SuperSelect/SuperSelect";
+import SuperRadio from "../h7/common/c6-SuperRadio/SuperRadio";
 import s from "./HW12.module.css";
 
 const themes = ['dark', 'red', 'some'];
@@ -10,15 +12,21 @@ function HW12() {
 
     return (
         <div className={s[theme]}>
-            <hr/>
+            <hr />
             <span className={s[theme + '-text']}>
                 homeworks 12
             </span>
+            <div>
+                <SuperRadio
+                    name={'radio'}
+                    options={themes}
+                />
+            </div>
 
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
 
-            <hr/>
+            <hr />
         </div>
     );
 }
